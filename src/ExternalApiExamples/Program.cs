63d047de930cf1f888466ae3a4876fc4a71a6acc
Jacek Stec (JTK) <JTK@kmd.dk>
2020-02-19 10:45:33 +1000

    prepared appsettings

--- src/ExternalApiExamples/Program.cs
+++ src/ExternalApiExamples/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using ExternalApiExamples.Clients;
+using Kmd.Logic.Identity.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Rest;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
-using ExternalApiExamples.Clients;
 
 namespace ExternalApiExamples
 {
@@ -8,8 +12,33 @@ namespace ExternalApiExamples
     {
         private static async Task Main(string[] args)
         {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .AddEnvironmentVariables()
+                    .Build()
+                    .Get<AppConfiguration>();
+
+                await Run(config).ConfigureAwait(false);
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+#pragma warning restore CA1031 // Do not catch general exception types
+        }
+
+        private static async Task Run(AppConfiguration configuration)
+        {
             Console.WriteLine("KMD Studica Examples");
-            var tokenProvider = new StudicaHostToHostTokenProvider("-- SECRET --");
+            using var httpClient = new HttpClient
+            {
+                BaseAddress = configuration.StudicaExternalApiBaseAddress
+            };
+            var tokenProvider = CreateLogicTokenProvider(configuration.TokenProviderOptions, httpClient);
             await new StudentsExample().Execute(tokenProvider);
             await new EmployeeExample().Execute(tokenProvider);
             await new RoomsExample().Execute(tokenProvider);
@@ -19,5 +48,16 @@ namespace ExternalApiExamples
             await new EducationalProgrammesExample().Execute(tokenProvider);
             await new SubjectCoursesExample().Execute(tokenProvider);
         }
+
+        private static ITokenProvider CreateLogicTokenProvider(TokenProviderOptions tokenProviderOptions, HttpClient httpClient) =>
+            new LogicTokenProviderFactory(
+                new LogicTokenProviderOptions
+                {
+                    AuthorizationScope = tokenProviderOptions.AuthorizationScope,
+                    AuthorizationTokenIssuer = tokenProviderOptions.AuthorizationTokenIssuer,
+                    ClientId = tokenProviderOptions.ClientId,
+                    ClientSecret = tokenProviderOptions.ClientSecret
+                })
+            .GetProvider(httpClient);
     }
 }

11da1ca7c3c98ed77bf065ce180a92c70c2a3943
Jacek Stec (JTK) <JTK@kmd.dk>
2020-02-19 10:45:33 +1000

    prepared appsettings

---
+++ src/ExternalApiExamples/AppConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using ExternalApiExamples.Clients;
+using System;
+
+namespace ExternalApiExamples
+{
+    internal class AppConfiguration
+    {
+        public TokenProviderOptions TokenProviderOptions { get; set; }
+        public Uri StudicaExternalApiBaseAddress { get; set; }
+    }
+}

60310012a73523baa62356a68189430d86bb6a7f
Jacek Stec (JTK) <JTK@kmd.dk>
2020-02-20 13:09:42 +1000

    removed property from AppConfiguraiton

--- src/ExternalApiExamples/AppConfiguration.cs
+++ src/ExternalApiExamples/AppConfiguration.cs
@@ -1,13 +1,10 @@
 ﻿using Kmd.Logic.Identity.Authorization;
-using System;
 
 namespace ExternalApiExamples
 {
     internal class AppConfiguration
     {
         public LogicTokenProviderOptions TokenProvider { get; set; }
-
-        public Uri StudicaExternalApiBaseAddress { get; set; }
 
         public string StudicaExternalApiKey { get; set; }
     }

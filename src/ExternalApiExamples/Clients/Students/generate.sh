4035d496d24daadb91b389417f7676e45c065edc
Jacek Stec (JTK) <JTK@kmd.dk>
2020-02-20 14:08:19 +1000

    simplified scripts to call remote openapi spec and deleted static files

--- src/ExternalApiExamples/Clients/Students/generate.sh
+++
@@ -1,2 +0,0 @@
-# curl -O http://localhost:5018/swagger/external/swagger.json
-autorest --input-file=swagger.json --output-folder=. --namespace=Kmd.Studica.Students.Client --csharp --add-credentials --payload-flattening-threshold=2
